a7f32ae4a66612993f63ce9bf9edb4f5e276f2a3
TheSquiddyLink <preston@happychaps.com>
2024-08-07 11:04:10 -0700

    Add Todos

--- script/util/constants.js
+++ script/util/constants.js
@@ -197,6 +197,7 @@ export const MAIN_WEAPONS = {
     ZinkMiniSplatling: new MainWeapon("Zink Mini Splatling", MAIN_TYPES.Splatling, "zink_mini_splatling", SUB_WEAPONS.ToxicMist, SPECIAL_WEAPONS.BigBubbler),
 }
 
+// TODO: Group/Filter by weapon type
 export const SORTED_WEAPONS = Object.entries(MAIN_WEAPONS)
   .sort((a, b) => a[1].type.localeCompare(b[1].type))
   .reduce((acc, [key, value]) => {
