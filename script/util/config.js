0fcfcc2d02842ec2e5bfa768c1419f34ba72632d
TheSquiddyLink <preston@happychaps.com>
2024-08-21 11:23:58 -0700

    Add JSDoc for config

--- script/util/config.js
+++ script/util/config.js
@@ -69,108 +69,55 @@ export class Package {
     }
 }
 
+/**
+ * Class for a config
+ * - Used in a {@link Package}
+ */
 export class Config {
-    /** @type {boolean} */
-    autoHide;
-
-    /** @type {number} */
-    hideLen;
-
-    /** @type {number} */
-    showLen;
-
-    /** @type {boolean} */
-    disableSound;
-
-    /** @type {number} */
-    iterations;
-
-    /** @type {boolean} */
-    disableAnimation;
-
-    /** @type {Team} */
-    teamColor;
-
-    /** @type {string} */
-    teamSide;
-
-    /** @type {boolean} */
-    editStars;
-
-    /** @type {boolean} */
-    displayStars;
-
-    /** @type {boolean} */
-    resultStars;
-
-    /** @type {number} */
-    starsFilter;
-
-    /** @type {boolean} */
-    exactStarsFilter;
-
-    /** @type {boolean} */
-    aniGenButton;
-
-    /** @type {boolean} */
-    autoURL;
-
-    /** @type {boolean} */
-    permaHide;
-
-    /** @type {boolean} */
-    rainbowBackground;
-
-    /** @type {boolean} */
-    rainbowButton;
-
-    /** @type {boolean} */
-    obsFriendly;
-
-    /** @type {boolean} */
-    invertSplat;
-
-    /** @type {boolean} */
-    hideHoverInfo;
-
-    /** @type {Color|null} */
-    customColor;
-
-    /** @type {Color|null} */
-    customBravoColor;
-
-    /** @type {number} */
-    weaponQueueSize;
-
-    /** @type {number} */
-    subQueueSize;
-
-    /** @type {number} */
-    specialQueueSize;
-
-    /** @type {number} */
-    typeQueueSize;
-
-    /** @type {boolean} */
-    smartGen;
-
-    /** @type {boolean} */
-    sideOrderMode;
-
-    /** @type {boolean} */
-    autoChipColor;
-
-    /** @type {boolean} */
-    averageChipColor;
-
-    /** @type {boolean} */
-    showChipResult;
+    /** @type {boolean} */ autoHide;
+    /** @type {number} */ hideLen;
+    /** @type {number} */ showLen;
+    /** @type {boolean} */ disableSound;
+    /** @type {number} */ iterations;
+    /** @type {boolean} */ disableAnimation;
+    /** @type {Team} */ teamColor;
+    /** @type {string} */ teamSide;
+    /** @type {boolean} */ editStars;
+    /** @type {boolean} */ displayStars;
+    /** @type {boolean} */ resultStars;
+    /** @type {number} */ starsFilter;
+    /** @type {boolean} */ exactStarsFilter;
+    /** @type {boolean} */ aniGenButton;
+    /** @type {boolean} */ autoURL;
+    /** @type {boolean} */ permaHide;
+    /** @type {boolean} */ rainbowBackground;
+    /** @type {boolean} */ rainbowButton;
+    /** @type {boolean} */ obsFriendly;
+    /** @type {boolean} */ invertSplat;
+    /** @type {boolean} */ hideHoverInfo;
+    /** @type {Color|null} */ customColor;
+    /** @type {Color|null} */ customBravoColor;
+    /** @type {number} */ weaponQueueSize;
+    /** @type {number} */ subQueueSize;
+    /** @type {number} */ specialQueueSize;
+    /** @type {number} */ typeQueueSize;
+    /** @type {boolean} */ smartGen;
+    /** @type {boolean} */ sideOrderMode;
+    /** @type {boolean} */ autoChipColor;
+    /** @type {boolean} */ averageChipColor;
+    /** @type {boolean} */ showChipResult;
     
+    /**
+     * The Metadata of the config
+     */
     metaData = {
         name: "",
         description: ""
     }
 
+    /**
+     * Set the default values of the config
+     */
     setDefault() {
         this.autoHide = false;
         this.hideLen = 2.5;
@@ -206,13 +153,24 @@ export class Config {
         this.showChipResult = true;
     }
     /**
-     * @param {string} property
+     * Check if the value at the property is currently the default value
+     * @param {string} property - Key of a property in the config
+     * @returns {boolean} - Whether the property is the default value
+     * @see {@link setDefault}
      */
     isDefault(property) {
         const defaultConfig = new Config();
         defaultConfig.setDefault();
         return this[property] == defaultConfig[property];
     }
+    /**
+     * Parse a JSON object into the config. Following cases are handled:
+     * - Custom Color: If the value is a string, it is parsed as a hex color code {@link Color.hex}
+     * - Custom Bravo Color: If the value is a string, it is parsed as a hex color code {@link Color.hex}
+     * - Team Color: If the value is a string, it is parsed as a team color object key {@link TEAMS}
+     * - Metadata: If the value is a string, it is parsed as a metadata object
+     * @param {Config} json - JSON object, expected to be a config JSON object
+     */
     parseJSON(json){
         const KEYS = Object.keys(this);
         let undefinedCount = 0;
@@ -248,11 +206,19 @@ export class Config {
             alert(`${undefinedCount} values were not valid for this config format. Please check the console for more information.`);
         }
     }
+    /**
+     * Parse a JSON object into the config.
+     * @see {@link Config#parseJSON} - Non static version of this method
+     */
     static parseJSON(json) {
         const config = new Config();
         config.parseJSON(json);
         return config;
     }
+    /**
+     * Clone the config into a new config
+     * @param {Config} config - Config to clone from
+     */
     cloneFrom(config) {
         for(let key in config) {
             if(config[key] == null) continue;
@@ -260,11 +226,24 @@ export class Config {
         }
     }
 
+    /**
+     * Set the metadata info of the config
+     * @param {String} name - Name of the config
+     * @param {String} description - Description of the config
+     * @see {@link metaData}
+     */
     setInfo(name, description) {
         this.metaData.name = name;
         this.metaData.description = description;
     }
     
+    /**
+     * Prepare the config for JSON serialization, handling the following:
+     * - Convert the team color to a string
+     * - Convert the custom color to a hex string
+     * - Convert the custom bravo color to a hex string
+     * @returns {Object} - JSON object of the config
+     */
     prepareJSON(){
         const JSON_CONFIG = structuredClone(this);
         console.log(JSON_CONFIG);
