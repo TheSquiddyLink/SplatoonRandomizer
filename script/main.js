f9dd84cbd80289d889bf638dc2ce109ca269f943
TheSquiddyLink <preston@happychaps.com>
2024-08-23 09:43:12 -0700

    Add will read frequently to canvas

--- script/main.js
+++ script/main.js
@@ -1833,7 +1833,7 @@ function toggleChipResult(){
  * @param {HTMLCanvasElement} canvas - The canvas to apply the color to
  */
 async function applyColor(color, imageID, canvas){
-    let ctx = canvas.getContext("2d");
+    let ctx = canvas.getContext("2d", { willReadFrequently: true });
     console.log(imageID)
     let image = document.getElementById(imageID);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
